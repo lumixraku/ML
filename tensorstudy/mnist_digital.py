9a84ceab53d0ef3e9de3533028604249d09c1fe1
luonan <luonan@bytedance.com>
2017-12-31 23:04:35 +0800

    numpy array to image
    Change-Id: I2b38d09808972d5bcf0b664314e51e0ce1828b13

--- tensorstudy/mnist_digital.py
+++ tensorstudy/mnist_digital.py
@@ -6,6 +6,8 @@
 import tensorflow as tf
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 import numpy as np
+from PIL import Image
+import struct
 from keras.datasets import mnist as kerasmnist
 mnist = input_data.read_data_sets('mnist_data/', one_hot=True)
 
@@ -33,12 +35,42 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))                 
 # 初始化所有变量
 init = tf.initialize_all_variables()
 
+
+def printImg(data):
+    """
+    data shape 为(784, ) 也就是一个1*784列
+    """
+    tmp = np.reshape(data, (28,28))
+    tmp = mnist.test.images[0]
+    tmp = [int(round(x)) for x in tmp]
+    print np.reshape(tmp, (28, 28))
+    # return tmp
+
+def showImg(data):
+
+
+# https: // stackoverflow.com / questions / 37558523 / converting - 2d - numpy - array - of - grayscale - values - to - a - pil - image
+#  The Image.fromarray(____, 'L') function seems to only work properly with an array of integers between 0 and 255. I use the np.uint8 function for this.
+    columns = 28
+    rows = 28
+    data = np.reshape(data, (columns, rows))
+    # print np.shape(data), data
+    mat = np.uint8(data * 255)
+    new_im = Image.fromarray(mat, 'L')
+    new_im.show()
+
+
 # 在一切操作之后，都用sess来run它们
 with tf.Session() as sess:
     sess.run(init)
 
     # mnist.test.images 测试数据10000条  每条都是 784个元素 的数组
     # print len(mnist.test.images), np.shape(mnist.test.images[0])
+    # 注意 label 的表示形式 array([0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.] 表示数字7  是因为在7这个位置的概率是1
+    # 之所以这么表示 可能是因为 softmax 吧 （毕竟这个的输出结果 所有概率和为1）
+    print(printImg(mnist.test.images[0]), mnist.test.labels[0], '===\n')
+    # showImg(mnist.test.images[0])
+
 
 
     for i in range(1000):       # 训练阶段，迭代1000次
@@ -49,9 +81,12 @@ with tf.Session() as sess:
 
         if i % 500 == 0:        # 每训练500次，测试一次
             # print("correct_prediction", sess.run(correct_prediction, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels}))
+
+            # mnist.test数据共有10000个 labels 也是10000个
             predictrs = sess.run(y_predictmax, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels})
-            print predictrs[0:100]
+
+            # print mnist.test.labels[0:100], '>--->', predictrs[0:100]
 
             actualrs = sess.run(y_actualmax, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels})
-            print actualrs[0:100]
-            # print("accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels}))
+            # print actualrs[0:100]
+            print("accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_actual: mnist.test.labels}))
